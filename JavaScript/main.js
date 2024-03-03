f58810fc19805fd8235eb9ac13b69c0f64f75354
Vitor Renato Soares de Souza <vitorrenatosouza33@gmail.com>
2024-03-03 13:56:23 -0300

    Atualizações pré-finais

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,4 +1,4 @@
-// Bem vindo com delay
+/* // Bem vindo com delay
 setTimeout(function () {
     let n1 = {
         nome: "???"
@@ -11,5 +11,35 @@ setTimeout(function () {
 
 }, 300)
 ///////////////////////////////////////
+ */
+function mudancaNome() {
+    alert("Você não tem o nível necessário!")
+    i = 1
+    if (i = 1) {
+        setTimeout(function () {
+            alert(`Interferência de ??? iniciada.`)
+
+        }, 3000)
+        setTimeout(function () {
+            alert(`Você obteve o direito de inspecionar os status do alvo`)
+
+        }, 4500)
+
+        setTimeout(function () {
+            alteradados()
+
+        }, 4900)
+    }
+}
+function alteradados() {
+
+    let alvo = {
+        nome: document.getElementById("nome").innerHTML = "<b>Nome:</b> Afarteus",
+        idade: document.getElementById("idade").innerHTML = "<b>Idade:</b> 3822 Anos",
+            sobre: document.getElementById("sobre").innerHTML = "<b>Sobre:</b> Aquele que, mesmo diante do fim, lutou com todas as forças e conquistou o direito de almejar os céus.",
+                nivel: document.getElementById("nivel").innerHTML = "<b>Nível:</b> 1999+",
+                    titulo: document.getElementById("titulo").innerHTML = "<b>Título:</b> Conquistador de Mundos."
+    }
 
 
+}
