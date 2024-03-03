ab48c5c3abdf092882c5176348689f4d30a5bf56
Vitor Renato Soares de Souza <vitorrenatosouza33@gmail.com>
2024-03-03 12:46:22 -0300

    atualização do código referente aos exercícios 6, 7 e 8. Provavelmente...

---
+++ JavaScript/main.js
@@ -0,0 +1,15 @@
+// Bem vindo com delay
+setTimeout(function () {
+    let n1 = {
+        nome: "???"
+    }
+    alert(`Welcome player ${n1.nome
+        } !`)
+
+
+
+
+}, 300)
+///////////////////////////////////////
+
+
